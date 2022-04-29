f68c4b2e35c3723c7ef0e88aca875da135c6c0f5
K-Myasn <kmyasnikov@cybertonica.com>
2022-04-29 21:18:46 +0300

    eslint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './style/style.scss'
-import {App} from './components/App'
-import {createRoot} from 'react-dom/client'
+import { App } from './components/App'
+import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.getElementById('root'))
 root.render(<App />)
